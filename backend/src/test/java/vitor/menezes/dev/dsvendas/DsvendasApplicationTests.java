39be77ec7988e6e18c0f5537963adc323a1e5b7d
Vítor A. Menezes de Sousa <vitor.menezes.dev@gmail.com>
2021-09-06 21:14:39 -0300

    Project created

---
+++ backend/src/test/java/vitor/menezes/dev/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package vitor.menezes.dev.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
